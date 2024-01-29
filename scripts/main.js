94b86bcdb876762782535bfa0bd466c99c6f6e76
loudovsky <luileptitecossais@gmail.com>
2024-01-29 11:29:26 +0100

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,14 @@
 let addButton = document.querySelectorAll(".add");
 let basket = document.querySelector(".count")
 let title = document.querySelector("h3");
-let nbrItems = 0;
 let sum = 0;
 
 addButton.forEach(element => {
     element.addEventListener('click', function() {
-        nbrItems++;
-        console.log(nbrItems);
-        basket.innerText = nbrItems;
+        basket.innerText++;
     })
     element.addEventListener('click', function() {
-        let price = element.closest(".item").querySelector(".price").textContent;
+        let price = element.previousElementSibling.textContent;
         let parsed = parseInt(price);   
         sum += parsed;
         console.log(sum);
